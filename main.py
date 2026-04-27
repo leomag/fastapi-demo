2fd7c762ac955ef45b30909358610a5764897365
Ilya Isaev <ivisaev@itmo.ru>
2026-04-28 00:11:31 +0300

    fix: fix routing

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, FastAPI
 
-from app.api.v1.operations import router as operations_router
-from app.api.v1.predict import router as predict_router
-from app.api.v1.users import router as users_router
-from app.api.v1.wallets import router as wallets_router
+from app.api.v2.operations import router as operations_router
+from app.api.v2.users import router as users_router
+from app.api.v2.wallets import router as wallets_router
 from app.database import Base, engine
 
 app = FastAPI()
@@ -12,6 +11,5 @@ router = APIRouter(prefix="/api/v2")
 app.include_router(wallets_router, tags=["wallets"])
 app.include_router(operations_router, tags=["operations"])
 app.include_router(users_router, tags=["users"])
-app.include_router(predict_router, tags=["predict"])
 
 Base.metadata.create_all(bind=engine)
